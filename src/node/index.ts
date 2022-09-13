a0bd6edfc33021a9ad77a9c60a77a412da80edc9
胡文彬 <huwenbin.tyreal@bytedance.com>
2022-09-13 23:42:01 +0800

    feat: update to expression.js parseArrowExpression

--- src/node/index.ts
+++ src/node/index.ts
@@ -49,11 +49,36 @@ export type Node =
   ImportDefaultSpecifier |
   ImportSpecifier |
   SequenceExpression |
+  YieldExpression |
   AssignmentExpression |
   ConditionalExpression |
   LogicalExpression |
-  BinaryExpression
-
+  BinaryExpression |
+  UpdateExpression |
+  UnaryExpression |
+  ChainExpression |
+  MemberExpression |
+  CallExpression |
+  TaggedTemplateExpression |
+  ThisExpression |
+  Super |
+  Literal |
+  ParenthesizedExpression |
+  ArrayExpression |
+  Identifier |
+  MetaProperty |
+  ImportExpression |
+  NewExpression |
+  TemplateElement |
+  TemplateLiteral |
+  ObjectExpression |
+  SpreadElement |
+  Property |
+  PrivateIdentifier |
+  ObjectPattern |
+  ArrayPattern |
+  RestElement |
+  AssignmentPattern
 
 export interface Program extends BaseNodeProps {
   type: 'Program'
@@ -303,6 +328,12 @@ export interface SequenceExpression extends BaseNodeProps {
   expressions: Array<Node>
 }
 
+export interface YieldExpression extends BaseNodeProps {
+  type: 'YieldExpression'
+  delegate: boolean
+  argument: Node | null
+}
+
 export interface AssignmentExpression extends BaseNodeProps {
   type: 'AssignmentExpression'
   operator: string
@@ -327,6 +358,158 @@ export interface LogicalExpression extends BaseNodeProps {
 export interface BinaryExpression extends BaseNodeProps {
   type: 'BinaryExpression'
   operator: string
+  left: Node
+  right: Node
+}
+
+export interface UpdateExpression extends BaseNodeProps {
+  type: 'UpdateExpression'
+  operator: string
+  prefix: boolean
+  argument: Node
+}
+
+export interface UnaryExpression extends BaseNodeProps {
+  type: 'UnaryExpression'
+  operator: string
+  prefix: boolean
+  argument: Node
+}
+
+export interface ChainExpression extends BaseNodeProps {
+  type: 'ChainExpression'
+  expression: Node
+}
+
+export interface MemberExpression extends BaseNodeProps {
+  type: 'MemberExpression'
+  object: Node
+  property: Node
+  computed: boolean
+  optional?: boolean
+}
+
+export interface CallExpression extends BaseNodeProps {
+  type: 'CallExpression'
+  callee: Node
+  arguments: Node
+  optional?: boolean
+}
+
+export interface TaggedTemplateExpression extends BaseNodeProps {
+  type: 'TaggedTemplateExpression'
+  tag: Node
+  quasi: Node
+}
+
+export interface ThisExpression extends BaseNodeProps {
+  type: 'ThisExpression'
+}
+
+export interface Super extends BaseNodeProps {
+  type: 'Super'
+}
+
+export interface Literal extends BaseNodeProps {
+  type: 'Literal'
+  regex?: {
+    pattern: string
+    flags: string
+  }
+  value: string | null
+  raw: string
+  bigint?: string
+}
+
+export interface ParenthesizedExpression extends BaseNodeProps {
+  type: 'ParenthesizedExpression'
+  expression: Node
+}
+
+export interface ArrayExpression extends BaseNodeProps {
+  type: 'ArrayExpression'
+  elements: Array<Node | null>
+}
+
+export interface Identifier extends BaseNodeProps {
+  type: 'Identifier'
+  name: string
+}
+
+export interface MetaProperty extends BaseNodeProps {
+  type: 'MetaProperty'
+  meta: Node
+  property: Node
+}
+
+export interface ImportExpression extends BaseNodeProps {
+  type: 'ImportExpression'
+  source: Node
+}
+
+export interface NewExpression extends BaseNodeProps {
+  type: 'NewExpression'
+  callee: Node
+  arguments: Array<Node>
+}
+
+export interface TemplateElement extends BaseNodeProps {
+  type: 'TemplateElement'
+  value: {
+    raw: string
+    cooked: null | string
+  }
+  tail: boolean
+}
+
+export interface TemplateLiteral extends BaseNodeProps {
+  type: 'TemplateLiteral'
+  expressions: Array<Node>
+  quasis: Array<Node>
+}
+
+export interface ObjectExpression extends BaseNodeProps {
+  type: 'ObjectExpression'
+  properties: Array<Node>
+}
+
+export interface SpreadElement extends BaseNodeProps {
+  type: 'SpreadElement'
+  argument: Array<Node>
+}
+
+export interface Property extends BaseNodeProps {
+  type: 'Property'
+  method: boolean
+  shorthand: boolean
+  key: Node
+  computed?: boolean
+  kind: string
+  value: Node
+}
+
+export interface PrivateIdentifier extends BaseNodeProps {
+  type: 'PrivateIdentifier'
+  name: string
+}
+
+export interface ObjectPattern extends BaseNodeProps {
+  type: 'ObjectPattern'
+  properties: Array<Node>
+}
+
+export interface ArrayPattern extends BaseNodeProps {
+  type: 'ArrayPattern'
+  elements: Array<Node | null>
+}
+
+export interface RestElement extends BaseNodeProps {
+  type: 'RestElement'
+  argument: Array<Node>
+}
+
+export interface AssignmentPattern extends BaseNodeProps {
+  type: 'AssignmentPattern'
   left: Node
   right: Node
 }
