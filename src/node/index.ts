0e768d37b66401bc5cdeb4c68031313e8dea99f9
胡文彬 <huwenbin.tyreal@bytedance.com>
2022-09-14 23:43:27 +0800

    feat: finish Node type

--- src/node/index.ts
+++ src/node/index.ts
@@ -79,22 +79,104 @@ export type Node =
   ObjectPattern |
   ArrayPattern |
   RestElement |
-  AssignmentPattern
+  AssignmentPattern |
+  ArrowFunctionExpression |
+  AwaitExpression
+
+export type Expression =
+  | ArrayExpression
+  | AssignmentExpression
+  | BinaryExpression
+  | CallExpression
+  | ConditionalExpression
+  | FunctionExpression
+  | Identifier
+  | LogicalExpression
+  | MemberExpression
+  | NewExpression
+  | ObjectExpression
+  | SequenceExpression
+  | ParenthesizedExpression
+  | ThisExpression
+  | UnaryExpression
+  | UpdateExpression
+  | ArrowFunctionExpression
+  | ClassExpression
+  | MetaProperty
+  | Super
+  | TaggedTemplateExpression
+  | TemplateLiteral
+  | YieldExpression
+  | AwaitExpression
+
+export type Statement =
+  | BlockStatement
+  | BreakStatement
+  | ContinueStatement
+  | DebuggerStatement
+  | DoWhileStatement
+  | EmptyStatement
+  | ExpressionStatement
+  | ForInStatement
+  | ForStatement
+  | FunctionDeclaration
+  | IfStatement
+  | LabeledStatement
+  | ReturnStatement
+  | SwitchStatement
+  | ThrowStatement
+  | TryStatement
+  | VariableDeclaration
+  | WhileStatement
+  | WithStatement
+  | ClassDeclaration
+  | ExportAllDeclaration
+  | ExportDefaultDeclaration
+  | ExportNamedDeclaration
+  | ForOfStatement
+  | ImportDeclaration
+
+export type Pattern = AssignmentPattern | ArrayPattern | ObjectPattern
+
+export type LVal =
+  | Identifier
+  | MemberExpression
+  | RestElement
+  | AssignmentPattern
+  | ArrayPattern
+  | ObjectPattern
+
+
+export type PatternLike =
+  | Identifier
+  | RestElement
+  | AssignmentPattern
+  | ArrayPattern
+  | ObjectPattern
+
+export type Declaration =
+  | FunctionDeclaration
+  | VariableDeclaration
+  | ClassDeclaration
+  | ExportAllDeclaration
+  | ExportDefaultDeclaration
+  | ExportNamedDeclaration
+  | ImportDeclaration
 
 export interface Program extends BaseNodeProps {
   type: 'Program'
   sourceType: Options['sourceType']
-  body: Array<Node>
+  body: Array<Statement>
 }
 
 export interface BreakStatement extends BaseNodeProps {
   type: 'BreakStatement'
-  label: null | Node
+  label: null | Identifier
 }
 
 export interface ContinueStatement extends BaseNodeProps {
   type: 'ContinueStatement'
-  label: null | Node
+  label: null | Identifier
 }
 
 export interface DebuggerStatement extends BaseNodeProps {
@@ -103,96 +185,97 @@ export interface DebuggerStatement extends BaseNodeProps {
 
 export interface DoWhileStatement extends BaseNodeProps {
   type: 'DoWhileStatement'
-  body: Node
-  test: Node
+  body: Statement
+  test: Expression
 }
 
 export interface ForStatement extends BaseNodeProps {
   type: 'ForStatement'
-  init: Node | null
-  test: Node | null
-  update: Node | null
-  body: Node
+  init: VariableDeclaration | Expression | null
+  test: Expression | null
+  update: Expression | null
+  body: Statement
 }
 
 export interface ForInStatement extends BaseNodeProps {
   type: 'ForInStatement'
-  body: Node
-  left: Node
-  right: Node
+  body: Statement
+  left: VariableDeclaration | LVal
+  right: Expression
 }
 
 export interface ForOfStatement extends BaseNodeProps {
   type: 'ForOfStatement'
-  body: Node
-  left: Node
-  right: Node
+  body: Statement
+  left: VariableDeclaration | LVal
+  right: Expression
+  await: boolean
 }
 
 export interface IfStatement extends BaseNodeProps{
   type: 'IfStatement'
-  test: Node
-  consequent: Node
-  alternate: Node
+  test: Expression
+  consequent: Statement
+  alternate: Statement | null
 }
 
 export interface ReturnStatement extends BaseNodeProps{
   type: 'ReturnStatement'
-  argument: Node | null
+  argument: Expression | null
 }
 
 export interface SwitchCase extends BaseNodeProps{
   type: 'SwitchCase'
-  consequent: Array<Node>
-  test: Node | null
+  consequent: Array<Statement>
+  test: Expression | null
 }
 
 export interface SwitchStatement extends BaseNodeProps{
   type: 'SwitchStatement'
-  discriminant: Node
-  cases: Array<Node>
+  discriminant: Expression;
+  cases: Array<SwitchCase>;
 }
 
 export interface ThrowStatement extends BaseNodeProps{
   type: 'ThrowStatement'
-  argument: Node
+  argument: Expression
 }
 
 export interface TryStatement extends BaseNodeProps{
   type: 'TryStatement'
-  block: Node
-  handler: Node | null
-  finalizer : Node | null
+  block: BlockStatement
+  handler: CatchClause | null
+  finalizer : BlockStatement | null
 }
 
 export interface CatchClause extends BaseNodeProps{
   type: 'CatchClause'
-  param: Node | null
-  body: Node
+  param: Identifier | ArrayPattern | ObjectPattern | null
+  body: BlockStatement
 }
 
 export interface VariableDeclaration extends BaseNodeProps{
   type: 'VariableDeclaration'
-  declarations: Array<Node>
-  kind: string
+  declarations: Array<VariableDeclarator>
+  kind: 'var' | 'let' | 'const'
 }
 
 export interface VariableDeclarator extends BaseNodeProps{
   type: 'VariableDeclarator'
-  id: Node
-  init: Node | null
+  id: LVal
+  init: Expression | null
 }
 
 export interface WhileStatement extends BaseNodeProps{
   type: 'WhileStatement'
-  test: Node
-  body: Node
+  test: Expression
+  body: Statement
 }
 
 export interface WithStatement extends BaseNodeProps{
   type: 'WithStatement'
-  object: Node
-  body: Node
+  object: Expression
+  body: Statement
 }
 
 export interface EmptyStatement extends BaseNodeProps{
@@ -201,206 +284,230 @@ export interface EmptyStatement extends BaseNodeProps{
 
 export interface LabeledStatement extends BaseNodeProps{
   type: 'LabeledStatement'
-  body: Node
-  label: Node
+  body: Statement
+  label: Identifier
 }
 
 export interface ExpressionStatement extends BaseNodeProps{
   type: 'ExpressionStatement'
-  expression: Node
+  expression: Expression
 }
 
 export interface BlockStatement extends BaseNodeProps{
   type: 'BlockStatement'
-  body: Array<Node>
+  body: Array<Statement>
 }
 
 export interface FunctionDeclaration extends BaseNodeProps{
   type: 'FunctionDeclaration'
-  id: Node | null
-  generator: boolean
+  id: Identifier | null
+  generator?: boolean
   async: boolean
-  params: Array<Node>
+  params: Array<Identifier | Pattern | RestElement>
   expression: boolean
-  body: Node
+  body: BlockStatement
 }
 
 export interface FunctionExpression extends BaseNodeProps{
   type: 'FunctionExpression'
-  id: Node | null
-  generator: boolean
+  id: Identifier | null
+  generator?: boolean
   async: boolean
-  params: Array<Node>
+  params: Array<Identifier | Pattern | RestElement>
   expression: boolean
-  body: Node
+  body: BlockStatement
 }
 
 export interface ClassDeclaration extends BaseNodeProps{
   type: 'ClassDeclaration'
-  id: Node | null
-  superClass: Node | null
-  body: Node
+  id: Identifier | null
+  superClass: Expression | null
+  body: ClassBody
 }
 
 export interface ClassExpression extends BaseNodeProps{
   type: 'ClassExpression'
-  id: Node | null
-  superClass: Node | null
-  body: Node
+  id: Identifier | null
+  superClass: Expression | null
+  body: ClassBody
 }
 
 export interface ClassBody extends BaseNodeProps {
   type: 'ClassBody'
-  body: Array<Node>
+  body: Array<StaticBlock | Identifier | MethodDefinition | PropertyDefinition>
 }
 
 export interface MethodDefinition extends BaseNodeProps {
   type: 'MethodDefinition'
   static: boolean
   computed: boolean
-  key: Node
-  kind: string
-  value: Node
+  key: Identifier | Literal | Expression
+  kind: "get" | "set" | "method" | "constructor"
+  value: FunctionExpression | null
 }
 
 export interface PropertyDefinition extends BaseNodeProps {
   type: 'PropertyDefinition'
   static: boolean
   computed: boolean
-  key: Node
-  value: Node | null
+  key: Identifier | Literal | Expression
+  value: Expression | null
 }
 
 export interface StaticBlock extends BaseNodeProps {
   type: 'StaticBlock'
-  body: Array<Node>
+  body: Array<Statement>
 }
 
 export interface ExportAllDeclaration extends BaseNodeProps {
   type: 'ExportAllDeclaration'
-  exported: Node | null
-  source: Node
+  exported: Identifier | Literal | null
+  source: Literal
 }
 
 export interface ExportDefaultDeclaration extends BaseNodeProps {
   type: 'ExportDefaultDeclaration'
-  declaration: Node
+  declaration: FunctionDeclaration | ClassDeclaration | Expression
 }
 
 export interface ExportNamedDeclaration extends BaseNodeProps {
   type: 'ExportNamedDeclaration'
-  declaration: Node | null
-  specifiers: Array<Node>
-  source: Node | null
+  declaration: Declaration | null
+  specifiers: Array<ExportSpecifier>
+  source: Literal | null
 }
 
 export interface ExportSpecifier extends BaseNodeProps {
   type: 'ExportSpecifier'
-  local: Node
-  exported: Node
+  local: Identifier
+  exported: Identifier | Literal
 }
 
 export interface ImportDeclaration extends BaseNodeProps {
   type: 'ImportDeclaration'
-  local: Node
-  exported: Node
-  specifiers: Array<Node>
-  source: Node
+  specifiers: Array<ImportSpecifier | ImportDefaultSpecifier | ImportNamespaceSpecifier>
+  source: Literal
 }
 
 export interface ImportDefaultSpecifier extends BaseNodeProps {
   type: 'ImportDefaultSpecifier'
-  local: Node
+  local: Identifier
 }
 
 export interface ImportNamespaceSpecifier extends BaseNodeProps {
   type: 'ImportNamespaceSpecifier'
-  local: Node
+  local: Identifier
 }
 
 export interface ImportSpecifier extends BaseNodeProps {
   type: 'ImportSpecifier'
-  local: Node
-  imported: Node
+  local: Identifier
+  imported: Identifier
 }
 
 export interface SequenceExpression extends BaseNodeProps {
   type: 'SequenceExpression'
-  expressions: Array<Node>
+  expressions: Array<Expression>
 }
 
 export interface YieldExpression extends BaseNodeProps {
   type: 'YieldExpression'
   delegate: boolean
-  argument: Node | null
+  argument: Expression | null
 }
 
 export interface AssignmentExpression extends BaseNodeProps {
   type: 'AssignmentExpression'
   operator: string
-  left: Node
-  right: Node
+  left: LVal
+  right: Expression
 }
 
 export interface ConditionalExpression extends BaseNodeProps {
   type: 'ConditionalExpression'
-  test: Node
-  consequent: Node
-  alternate: Node
+  test: Expression
+  consequent: Expression
+  alternate: Expression
 }
 
 export interface LogicalExpression extends BaseNodeProps {
   type: 'LogicalExpression'
-  operator: string
-  left: Node
-  right: Node
+  operator:
+    | "||"
+    | "&&"
+    | "??"
+  left: Expression
+  right: Expression
 }
 
 export interface BinaryExpression extends BaseNodeProps {
   type: 'BinaryExpression'
-  operator: string
-  left: Node
-  right: Node
+  operator:
+    | "+"
+    | "-"
+    | "/"
+    | "%"
+    | "*"
+    | "**"
+    | "&"
+    | "|"
+    | ">>"
+    | ">>>"
+    | "<<"
+    | "^"
+    | "=="
+    | "==="
+    | "!="
+    | "!=="
+    | "in"
+    | "instanceof"
+    | ">"
+    | "<"
+    | ">="
+    | "<="
+    | "|>"
+  left: Expression
+  right: Expression
 }
 
 export interface UpdateExpression extends BaseNodeProps {
   type: 'UpdateExpression'
-  operator: string
+  operator: "++" | "--"
+  argument: Expression
   prefix: boolean
-  argument: Node
 }
 
 export interface UnaryExpression extends BaseNodeProps {
   type: 'UnaryExpression'
-  operator: string
+  operator: 'void' | 'throw' | 'delete' | '!' | '+' | '-' | '~' | 'typeof'
   prefix: boolean
-  argument: Node
+  argument: Expression
 }
 
 export interface ChainExpression extends BaseNodeProps {
   type: 'ChainExpression'
-  expression: Node
+  expression: ArrowFunctionExpression
 }
 
 export interface MemberExpression extends BaseNodeProps {
   type: 'MemberExpression'
-  object: Node
-  property: Node
+  object: Expression
+  property: Expression | Identifier | PrivateIdentifier
   computed: boolean
   optional?: boolean
 }
 
 export interface CallExpression extends BaseNodeProps {
   type: 'CallExpression'
-  callee: Node
-  arguments: Node
+  callee: Expression
+  arguments: Array<Expression | SpreadElement | null>
   optional?: boolean
 }
 
 export interface TaggedTemplateExpression extends BaseNodeProps {
   type: 'TaggedTemplateExpression'
-  tag: Node
-  quasi: Node
+  tag: Expression
+  quasi: TemplateLiteral
 }
 
 export interface ThisExpression extends BaseNodeProps {
@@ -413,23 +520,23 @@ export interface Super extends BaseNodeProps {
 
 export interface Literal extends BaseNodeProps {
   type: 'Literal'
+  value: string
+  raw: string
+  bigint?: string
   regex?: {
     pattern: string
     flags: string
   }
-  value: string | null
-  raw: string
-  bigint?: string
 }
 
 export interface ParenthesizedExpression extends BaseNodeProps {
   type: 'ParenthesizedExpression'
-  expression: Node
+  expression: Expression
 }
 
 export interface ArrayExpression extends BaseNodeProps {
   type: 'ArrayExpression'
-  elements: Array<Node | null>
+  elements: Array<Expression | SpreadElement | null>
 }
 
 export interface Identifier extends BaseNodeProps {
@@ -439,19 +546,19 @@ export interface Identifier extends BaseNodeProps {
 
 export interface MetaProperty extends BaseNodeProps {
   type: 'MetaProperty'
-  meta: Node
-  property: Node
+  meta: Identifier
+  property: Identifier
 }
 
 export interface ImportExpression extends BaseNodeProps {
   type: 'ImportExpression'
-  source: Node
+  source: Expression
 }
 
 export interface NewExpression extends BaseNodeProps {
   type: 'NewExpression'
-  callee: Node
-  arguments: Array<Node>
+  callee: Expression
+  arguments: Array<Expression | SpreadElement | null>
 }
 
 export interface TemplateElement extends BaseNodeProps {
@@ -465,28 +572,28 @@ export interface TemplateElement extends BaseNodeProps {
 
 export interface TemplateLiteral extends BaseNodeProps {
   type: 'TemplateLiteral'
-  expressions: Array<Node>
-  quasis: Array<Node>
+  expressions: Array<Expression>
+  quasis: Array<TemplateElement>
 }
 
 export interface ObjectExpression extends BaseNodeProps {
   type: 'ObjectExpression'
-  properties: Array<Node>
+  properties: Array<Property | SpreadElement | RestElement>
 }
 
 export interface SpreadElement extends BaseNodeProps {
   type: 'SpreadElement'
-  argument: Array<Node>
+  argument: Expression
 }
 
 export interface Property extends BaseNodeProps {
   type: 'Property'
   method: boolean
   shorthand: boolean
-  key: Node
+  key: Identifier | Literal | Expression
   computed?: boolean
   kind: string
-  value: Node
+  value: Expression
 }
 
 export interface PrivateIdentifier extends BaseNodeProps {
@@ -496,21 +603,39 @@ export interface PrivateIdentifier extends BaseNodeProps {
 
 export interface ObjectPattern extends BaseNodeProps {
   type: 'ObjectPattern'
-  properties: Array<Node>
+  properties: Array<Property | SpreadElement | RestElement>
 }
 
 export interface ArrayPattern extends BaseNodeProps {
   type: 'ArrayPattern'
-  elements: Array<Node | null>
+  elements: Array<PatternLike | LVal | null>
 }
 
 export interface RestElement extends BaseNodeProps {
   type: 'RestElement'
-  argument: Array<Node>
+  argument: LVal
 }
 
 export interface AssignmentPattern extends BaseNodeProps {
   type: 'AssignmentPattern'
-  left: Node
-  right: Node
+  left:
+    | Identifier
+    | ObjectPattern
+    | ArrayPattern
+    | MemberExpression
+  right: Expression
+}
+
+export interface ArrowFunctionExpression extends BaseNodeProps {
+  type: 'ArrowFunctionExpression'
+  async: boolean
+  params: Array<Identifier | Pattern | RestElement>
+  expression: boolean
+  body: BlockStatement | Expression
+  generator?: boolean
+}
+
+export interface AwaitExpression extends BaseNodeProps {
+  type: 'AwaitExpression'
+  argument: Expression
 }
